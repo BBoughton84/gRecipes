699a173bf1bb5bd59aa74417d21f08150c505669
Bill Boughton <boughton.william@gmail.com>
2017-03-14 14:10:34 -0600

    added some routes

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,5 +1,48 @@
 const router = require('express').Router()
 const knex = require('../db/knex.js')
 
+router.get('/', (req, res) => {
+  knex('recipe')
+    .then(result => {
+      res.send(result)
+    })
+})
+
+router.get('/:id', (req, res) => {
+  var recipeId = req.params.id
+  knex('recipe').where('id', recipeId)
+    .then(result => {
+      res.send(result)
+    })
+})
+
+router.post('/', (req, res) => {
+  var recipeName = req.body.name
+  var recipeDesc = req.body.description
+  var authorName = req.body.author_name
+  var imageURL = req.body.image_URL
+  var stepArray = req.body.stepArray
+
+  knex('author').where('name', authorName)
+    .then(result => {
+      if (result[0].name == authorName) {
+        knex('recipe').insert({name:recipeName, author_id:result[0].id, description:recipeDesc, image_URL:imageURL}).returning(['id', 'name', 'author_id', 'description', 'image_URL'])
+          .then(newRecipe => {
+                res.send(newRecipe)
+          })
+      }
+    })
+    .catch(result => {
+      knex('author').insert({name:authorName}).returning('id')
+        .then(result => {
+          knex('recipe').insert({name:recipeName, author_id:result[0], description:recipeDesc, image_URL:imageURL}).returning(['id', 'name', 'author_id', 'description', 'image_URL'])
+            .then(sendingBackNewRecipeNewUser => {
+              res.send(sendingBackNewRecipeNewUser)
+            })
+        })
+    })
+})
+
+
 
 module.exports = router
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,4 +1,50 @@
 const router = require('express').Router()
 const knex = require('../db/knex.js')
 
+router.get('/all/:id', (req, res) => {
+  var reviewId = req.params.id
+  knex('review').where('recipe_id', reviewId)
+    .then(result => {
+      res.send(result)
+    })
+})
+
+router.get('/specific/:id', (req, res) => {
+  var reviewId = req.params.id
+  knex('review').where('review_id', reviewId)
+    .then(result => {
+      res.send(result)
+    })
+})
+
+router.post('/', (req, res) => {
+  var postId = req.body.recipe_id
+  var postReview = req.body.body
+  var postRating = req.body.rating
+  var authorName = req.body.author_name
+
+  knex('author').where('name', authorName)
+    .then(result => {
+      if (result[0].name == authorName) {
+        knex('review').insert({recipe_id:postId, author_id:result[0].id, body:postReview, rating:postRating})
+          .then(result => {
+            knex('review').where('body', postReview)
+              .then(newCommentToSendback => {
+                res.send(newCommentToSendback)
+              })
+          })
+      }
+    })
+    .catch(result => {
+      knex('author').insert({name:authorName}).returning('id')
+        .then(result => {
+          knex('review').insert({recipe_id:postId, author_id:result[0], body:postReview, rating:postRating}).returning(['id', 'recipe_id', 'author_id', 'body', 'rating'])
+            .then(sendingBackNewCommentNewUser => {
+              res.send(sendingBackNewCommentNewUser)
+            })
+        })
+    })
+})
+
+
 module.exports = router
